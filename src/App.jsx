6385c0307922bc9ea8b493e65a898ba232e5c0a8
Artem <64410376+Artiss90@users.noreply.github.com>
2021-01-21 21:20:28 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/* eslint react/prop-types: 1 */
+
+class App extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
