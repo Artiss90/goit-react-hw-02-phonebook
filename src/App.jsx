790a435b4cd1232268e0c5cf1fd6ab1a106683b2
Artem <64410376+Artiss90@users.noreply.github.com>
2021-01-22 00:14:29 +0200

    task-2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { number } from 'prop-types';
 import { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
@@ -13,8 +14,31 @@ class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: '',
+    number: '',
   };
   nameInputId = uuidv4();
+  numberInputId = uuidv4();
+
+  addContact = ({ name, number }) => {
+    const phoneContact = {
+      id: uuidv4(),
+      name: name,
+      number: number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [phoneContact, ...contacts],
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //  const {name, number} = this.state
+    console.log(this.state);
+    this.addContact(this.state);
+
+    // this.reset();
+  };
 
   handleChange = e => {
     const { name, value } = e.currentTarget;
@@ -23,11 +47,11 @@ class App extends Component {
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
     return (
       <>
         <h2>Phonebook</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor={this.nameInputId}>
             Name
             <input
@@ -38,11 +62,23 @@ class App extends Component {
               id={this.nameInputId}
             />
           </label>
+          <label htmlFor={this.numberInputId}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              id={this.numberInputId}
+            />
+          </label>
           <button type="submit">Add contact</button>
           <ul>
             <h2>Contacts</h2>
             {contacts.map(contact => (
-              <li key={contact.id}>{contact.name}</li>
+              <li key={contact.id}>
+                {contact.name}: {contact.number}
+              </li>
             ))}
           </ul>
         </form>
